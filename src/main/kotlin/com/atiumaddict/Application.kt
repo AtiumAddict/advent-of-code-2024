17d5d554cf17aa675cd6175dba14747f290f1c96
Alexandros Lagios <alexandroslagios@gmail.com>
2024-12-04 07:55:38 +0200

    feat: :sparkles: Day 4
    Took 1 hour 0 minutes

--- src/main/kotlin/com/atiumaddict/Application.kt
+++ src/main/kotlin/com/atiumaddict/Application.kt
@@ -2,24 +2,25 @@ package com.atiumaddict
 
 import com.atiumaddict.adventofcode.day02.solveDay02First
 import com.atiumaddict.adventofcode.day02.solveDay02Second
-import com.atiumaddict.day03.solveDay03First
-import com.atiumaddict.day03.solveDay03Second
+import com.atiumaddict.day04.solveDay04First
+import com.atiumaddict.day04.solveDay04Second
 import io.ktor.server.application.*
 import java.io.File
 import kotlin.system.measureTimeMillis
 
 fun main() {
     println("Advent of Code 2024")
-    val lines = File("src/main/kotlin/com/atiumaddict/day03/finalInput.txt").readLines()
-    val time1 = measureTimeMillis {
-        println("Day 03 First Solution: ${solveDay03First(lines)}")
-    }
-    println("Time taken for Day 03 First Solution: $time1 ms")
+    val lines = File("src/main/kotlin/com/atiumaddict/day04/finalInput.txt").readLines()
 
-    val time2 = measureTimeMillis {
-        println("Day 03 Second Solution: ${solveDay03Second(lines)}")
+    val time1 = measureTimeMillis {
+        println("Day 04 First Solution: ${solveDay04First(lines)}")
     }
-    println("Time taken for Day 03 Second Solution: $time2 ms")
+    println("Time taken for Day 04 First Solution: $time1 ms")
+    val time2 = measureTimeMillis {
+        println("Day 04 Second Solution: ${solveDay04Second(lines)}")
+    }
+
+    println("Time taken for Day 04 Second Solution: $time2 ms")
 
 }
 
---
+++ src/main/kotlin/com/atiumaddict/day04/Day04.kt
@@ -0,0 +1,82 @@
+package com.atiumaddict.day04
+
+val letterSequence = listOf('X', 'M', 'A', 'S')
+
+fun solveDay04First(lines: List<String>): Long {
+    val grid = lines.map { it.toList() }
+    var result = 0L
+    for ((i, row) in grid.withIndex()) {
+        for ((j, char) in row.withIndex()) {
+            if (char == 'X') {
+                result += searchFromX(grid, i, j)
+            }
+        }
+    }
+    return result
+}
+
+fun searchFromX(grid: List<List<Char>>, row: Int, column: Int): Int {
+    val possibleNextSteps = getNextSteps(row, column)
+    var count = 0
+    for ((nextRow, nextCol) in possibleNextSteps) {
+        if (nextRow in grid.indices && nextCol in grid[nextRow].indices && grid[nextRow][nextCol] == 'M') {
+            if (searchForNextLetter(grid, nextRow, nextCol, possibleNextSteps.indexOf(Pair(nextRow, nextCol)))) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+fun searchForNextLetter(grid: List<List<Char>>, row: Int, column: Int, direction: Int): Boolean {
+    if (grid[row][column] == 'S') {
+        return true
+    }
+    val letterIndex = letterSequence.indexOf(grid[row][column])
+    val possibleNextSteps = getNextSteps(row, column)
+    val (nextRow, nextCol) = possibleNextSteps[direction]
+    if (nextRow in grid.indices && nextCol in grid[nextRow].indices && grid[nextRow][nextCol] == letterSequence[letterIndex + 1]) {
+        return searchForNextLetter(grid, nextRow, nextCol, direction)
+    }
+    return false
+}
+
+private fun getNextSteps(row: Int, column: Int): List<Pair<Int, Int>> {
+    return listOf(
+        Pair(row, column + 1),
+        Pair(row + 1, column),
+        Pair(row, column - 1),
+        Pair(row - 1, column),
+        Pair(row + 1, column + 1),
+        Pair(row - 1, column - 1),
+        Pair(row + 1, column - 1),
+        Pair(row - 1, column + 1),
+    )
+}
+
+fun solveDay04Second(lines: List<String>): Long {
+    val grid = lines.map { it.toList() }
+    var result = 0L
+    for ((i, row) in grid.withIndex()) {
+        for ((j, char) in row.withIndex()) {
+            if (i < 1 || i >= grid.size - 1 || j < 1 || j >= grid[i].size - 1) {
+                continue
+            }
+            if (char == 'A') {
+                val isXMAS = searchFromA(grid, i, j)
+                if (isXMAS) result++
+            }
+        }
+    }
+    return result
+}
+
+fun searchFromA(grid: List<List<Char>>, row: Int, column: Int): Boolean {
+    val leftLine = Pair(grid[row - 1][column - 1], grid[row + 1][column + 1])
+    val rightLine = Pair(grid[row - 1][column + 1], grid[row + 1][column - 1])
+    return isMASPair(leftLine) && isMASPair(rightLine)
+}
+
+private fun isMASPair(line: Pair<Char, Char>) =
+    (line.first == 'M' && line.second == 'S') || (line.second == 'M' && line.first == 'S')
+

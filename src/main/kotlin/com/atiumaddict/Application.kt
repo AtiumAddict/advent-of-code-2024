5416de8e60bd4b5f588884f37835f6a381ba4cf0
Alexandros Lagios <alexandroslagios@gmail.com>
2024-12-10 08:04:27 +0200

    feat: :sparkles: Day 10
    Took 38 minutes

--- src/main/kotlin/com/atiumaddict/Application.kt
+++ src/main/kotlin/com/atiumaddict/Application.kt
@@ -1,7 +1,7 @@
 package com.atiumaddict
 
-import com.atiumaddict.day09.solveDay09First
-import com.atiumaddict.day09.solveDay09Second
+import com.atiumaddict.day10.solveDay10First
+import com.atiumaddict.day10.solveDay10Second
 import java.io.File
 
 fun main() {
@@ -10,29 +10,29 @@ fun main() {
 
 fun solve() {
     println("Advent of Code 2024")
-//    val lines = File("src/main/kotlin/com/atiumaddict/day09/testInput.txt").readLines()
-    val lines = File("src/main/kotlin/com/atiumaddict/day09/finalInput.txt").readLines()
+//    val lines = File("src/main/kotlin/com/atiumaddict/day10/testInput.txt").readLines()
+    val lines = File("src/main/kotlin/com/atiumaddict/day10/finalInput.txt").readLines()
 
-    val iterations = 100
+    val iterations = 10000
     var totalTime1 = 1L
     var totalTime2 = 0L
 
     repeat(iterations) {
         val start1 = System.nanoTime()
-        solveDay09First(lines)
+        solveDay10First(lines)
         totalTime1 += System.nanoTime() - start1
 
         val start2 = System.nanoTime()
-        solveDay09Second(lines)
+        solveDay10Second(lines)
         totalTime2 += System.nanoTime() - start2
     }
 
     val averageTime1 = totalTime1 / iterations
     val averageTime2 = totalTime2 / iterations
 
-    println("Day 09 First Part: ${solveDay09First(lines)}")
-    println("Day 09 Second Part: ${solveDay09Second(lines)}")
-    println("Average time taken for Day 09 (without reading the file) in $iterations iterations:")
+    println("Day 10 First Part: ${solveDay10First(lines)}")
+    println("Day 10 Second Part: ${solveDay10Second(lines)}")
+    println("Average time taken for Day 10 (without reading the file) in $iterations iterations:")
     println("First Solution: ${String.format("%,d", averageTime1)} ns")
     println("Second Solution: ${String.format("%,d", averageTime2)} ns")
 }
---
+++ src/main/kotlin/com/atiumaddict/day10/Day10.kt
@@ -0,0 +1,83 @@
+package com.atiumaddict.day10
+
+fun solveDay10First(lines: List<String>): Long {
+    return lines.mapIndexed { r, row ->
+        row.withIndex()
+            .filter { it.value == '0' }
+            .map { col ->
+                // For each trailhead, find unique 9s it can reach
+                findNines(Pair(r, col.index), -1, mutableSetOf(), lines).size
+            }
+    }.flatten().sum().toLong()
+}
+
+
+val directions = listOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
+
+fun findNines(
+    current: Pair<Int, Int>,
+    previousHeight: Int,
+    visited: MutableSet<Pair<Int, Int>>,
+    lines: List<String>
+): Set<Pair<Int, Int>> {
+    val (r, c) = current
+    if (r !in lines.indices || c !in lines[r].indices ||
+        current in visited ||
+        lines[r][c].digitToInt() != previousHeight + 1
+    ) {
+        return emptySet()
+    }
+
+    visited.add(current)
+    val ninePositions = if (lines[r][c] == '9') setOf(current) else emptySet()
+
+    return ninePositions + directions.flatMap { (dr, dc) ->
+        findNines(Pair(r + dr, c + dc), lines[r][c].digitToInt(), visited, lines)
+    }
+}
+
+fun solveDay10Second(lines: List<String>): Long {
+    return lines.mapIndexed { r, row ->
+        row.withIndex()
+            .filter { it.value == '0' }
+            .map { col ->
+                // For each trailhead, find unique 9s it can reach
+                countTrails(Pair(r, col.index), -1, lines, 0)
+            }
+    }.flatten().sum().toLong()
+}
+
+fun countTrails(
+    current: Pair<Int, Int>,
+    previousHeight: Int,
+    lines: List<String>,
+    countOfNines: Int,
+    comingFrom: Pair<Int, Int> = Pair(-1, -1)
+): Int {
+    if (current.first !in lines.indices || current.second !in 0 until lines[0].length ||
+        lines[current.first][current.second].digitToInt() != previousHeight + 1
+    ) {
+        return 0
+    }
+
+    if (lines[current.first][current.second] == '9') {
+        return countOfNines + 1
+    }
+
+    var newCountOfNines = countOfNines
+    for (direction in directions) {
+        val nextPosition = Pair(current.first + direction.first, current.second + direction.second)
+        if (nextPosition == comingFrom) {
+            continue
+        }
+        newCountOfNines += countTrails(
+            Pair(current.first + direction.first, current.second + direction.second),
+            lines[current.first][current.second].digitToInt(),
+            lines,
+            0,
+            current,
+        )
+    }
+    return newCountOfNines
+}
+

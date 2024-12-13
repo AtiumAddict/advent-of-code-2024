8a4ab2baa84ed3df80c5d8214fbd5bfc8e0c27f8
Alexandros Lagios <alexandroslagios@gmail.com>
2024-12-13 23:43:30 +0200

    feat: :sparkles: Day 13 with help from Cramer's rule
    https://www.youtube.com/watch?v=jBsC34PxzoM
    
    Took 46 minutes
    
    Took 2 minutes

--- src/main/kotlin/com/atiumaddict/Application.kt
+++ src/main/kotlin/com/atiumaddict/Application.kt
@@ -1,7 +1,7 @@
 package com.atiumaddict
 
-import com.atiumaddict.day12.solveDay12First
-import com.atiumaddict.day12.solveDay12Second
+import com.atiumaddict.day13.solveDay13First
+import com.atiumaddict.day13.solveDay13Second
 import java.io.File
 
 fun main() {
@@ -10,29 +10,29 @@ fun main() {
 
 fun solve() {
     println("Advent of Code 2024")
-    val lines = File("src/main/kotlin/com/atiumaddict/day12/testInput.txt").readLines()
-//    val lines = File("src/main/kotlin/com/atiumaddict/day12/finalInput.txt").readLines()
+//    val lines = File("src/main/kotlin/com/atiumaddict/day13/testInput.txt").readLines()
+    val lines = File("src/main/kotlin/com/atiumaddict/day13/finalInput.txt").readLines()
 
-    val iterations = 1
+    val iterations = 10000
     var totalTime1 = 1L
     var totalTime2 = 0L
 
     repeat(iterations) {
         val start1 = System.nanoTime()
-        solveDay12First(lines)
+        solveDay13First(lines)
         totalTime1 += System.nanoTime() - start1
 
         val start2 = System.nanoTime()
-        solveDay12Second(lines)
+        solveDay13Second(lines)
         totalTime2 += System.nanoTime() - start2
     }
 
     val averageTime1 = totalTime1 / iterations
     val averageTime2 = totalTime2 / iterations
 
-    println("Day 12 First Part: ${solveDay12First(lines)}")
-    println("Day 12 Second Part: ${solveDay12Second(lines)}")
-    println("Average time taken for Day 12 (without reading the file) in $iterations iterations:")
+    println("Day 13 First Part: ${solveDay13First(lines)}")
+    println("Day 13 Second Part: ${solveDay13Second(lines)}")
+    println("Average time taken for Day 13 (without reading the file) in $iterations iterations:")
     println("First Solution: ${String.format("%,d", averageTime1)} ns")
     println("Second Solution: ${String.format("%,d", averageTime2)} ns")
 }
---
+++ src/main/kotlin/com/atiumaddict/day13/Day13.kt
@@ -0,0 +1,119 @@
+package com.atiumaddict.day13
+
+fun solveDay13First(lines: List<String>): Long {
+    val machines = getMachines(lines)
+    var totalTokens = 0L
+    for (machine in machines) {
+        val tokens = calculateTokensWithMath(machine)
+        if (tokens.first != 0L) {
+            totalTokens += (tokens.first * 3) + tokens.second
+        }
+    }
+    return totalTokens
+}
+
+class Machine(var prize: Pair<Long, Long>, val buttonA: Pair<Long, Long>, val buttonB: Pair<Long, Long>) {
+    override fun toString(): String {
+        return "Machine(prize=$prize, buttonA=$buttonA, buttonB=$buttonB)"
+    }
+}
+
+/**
+ * @deprecated This function is too slow for the second part of the problem.
+ */
+fun calculateTokensWithBruteForce(machine: Machine): Pair<Long, Long> {
+    var aTokens = 0L
+    var bTokens = 1L + machine.prize.first / machine.buttonB.first
+    while (aTokens < 100 && bTokens > 0) {
+        var clawPosition = findClawPosition(aTokens, bTokens, machine)
+        if (clawPosition == machine.prize) {
+            return Pair(aTokens, bTokens)
+        }
+        if (clawPosition.first < machine.prize.first && clawPosition.second < machine.prize.second) {
+            while (clawPosition.first < machine.prize.first && clawPosition.second < machine.prize.second) {
+                aTokens++
+                clawPosition = findClawPosition(aTokens, bTokens, machine)
+                if (clawPosition == machine.prize) {
+                    return Pair(aTokens, bTokens)
+                }
+            }
+        }
+        bTokens--
+    }
+    return Pair(0, 0)
+}
+
+private fun findClawPosition(
+    aTokens: Long,
+    bTokens: Long,
+    machine: Machine
+): Pair<Long, Long> {
+    var clawPositionX = aTokens * machine.buttonA.first + bTokens * machine.buttonB.first
+    var clawPositionY = aTokens * machine.buttonA.second + bTokens * machine.buttonB.second
+    return Pair(clawPositionX, clawPositionY)
+}
+
+val additive = 10000000000000
+fun solveDay13Second(lines: List<String>): Long {
+    val machines = getMachines(lines)
+    var totalTokens = 0L
+    for (machine in machines) {
+        machine.prize = Pair(machine.prize.first + additive, machine.prize.second + additive)
+        val tokens = calculateTokensWithMath(machine)
+        if (tokens.first != 0L) {
+            totalTokens += (tokens.first * 3) + tokens.second
+        }
+    }
+    return totalTokens
+}
+
+fun calculateTokensWithMath(machine: Machine): Pair<Long, Long> {
+    val buttonAX = machine.buttonA.first
+    val buttonAY = machine.buttonA.second
+    val buttonBX = machine.buttonB.first
+    val buttonBY = machine.buttonB.second
+    val prizeX = machine.prize.first
+    val prizeY = machine.prize.second
+    val divisor = buttonAX * buttonBY - buttonAY * buttonBX
+    val a = (prizeX * buttonBY - prizeY * buttonBX) / divisor
+    val b = (buttonAX * prizeY - buttonAY * prizeX) / divisor
+    return if (buttonAX * a + buttonBX * b == prizeX && buttonAY * a + buttonBY * b == prizeY) {
+        Pair(a, b)
+    } else Pair(0, 0)
+}
+
+fun getMachines(lines: List<String>): List<Machine> {
+    val machines = mutableListOf<Machine>()
+    var buttonA: Pair<Long, Long>? = null
+    var buttonB: Pair<Long, Long>? = null
+    var prize: Pair<Long, Long>? = null
+
+    lines.forEach { line ->
+        when {
+            line.startsWith("Button A:") -> {
+                val (x, y) = line.substringAfter("Button A:").split(",").map { it.trim().substring(2).toLong() }
+                buttonA = Pair(x, y)
+            }
+
+            line.startsWith("Button B:") -> {
+                val (x, y) = line.substringAfter("Button B:").split(",").map { it.trim().substring(2).toLong() }
+                buttonB = Pair(x, y)
+            }
+
+            line.startsWith("Prize:") -> {
+                val (x, y) = line.substringAfter("Prize:").split(",").map { it.trim().substring(2).toLong() }
+                prize = Pair(x, y)
+            }
+        }
+
+        if (buttonA != null && buttonB != null && prize != null) {
+            machines.add(Machine(prize!!, buttonA!!, buttonB!!))
+            buttonA = null
+            buttonB = null
+            prize = null
+        }
+    }
+
+    return machines
+}
+
